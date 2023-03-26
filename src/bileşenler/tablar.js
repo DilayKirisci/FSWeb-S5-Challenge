2018a5cfa5b58612dc00ad096c6dbf016a925624
Dilay Kirisci <isik.kirisci@gmail.com>
2023-03-26 13:41:48 +0300

    final version

--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,28 +1,35 @@
+import axios from "axios";
 const Tablar = (konu) => {
-  // GÖREV 3
-  // ---------------------
-  // Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
-  // Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
-  // fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
-  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">teknoloji</div>
-  // </div>
-  //
-}
+	let secilen = "";
+	const topicsDiv = document.createElement("div");
+	topicsDiv.classList.add("topics");
+	konu.forEach((item) => {
+		const tab = document.createElement("div");
+		tab.classList.add("tab");
+		tab.textContent = item;
+		topicsDiv.appendChild(tab);
+		tab.addEventListener("click", (e) => {
+			console.log(secilen);
+			return (secilen = e.target.textContent);
+		});
+	});
+	return topicsDiv;
+};
 
 const tabEkleyici = (secici) => {
-  // GÖREV 4
-  // ---------------------
-  // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
-  // Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
-  // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
-  // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
-  //
-}
+	// GÖREV 4
+	// ---------------------
+	// Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
+	// Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
+	// Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
+	// Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+	//
+	axios.get(`http://localhost:5001/api/konular`).then((response) => {
+		console.log(response.data.konular);
+		const elem = document.querySelector(secici);
+		const tablar = Tablar(response.data.konular);
+		elem.appendChild(tablar);
+	});
+};
 
-export { Tablar, tabEkleyici }
+export { Tablar, tabEkleyici };
